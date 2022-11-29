67e1cf8d3ff69ec2ded2ff4fc3a7cc2540d7a0d1
Gustaf Dipa <gustaf.dipa19080@student.ac.id>
2022-11-29 20:51:43 +0700

    cors'
    '

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const db = require('./config/db')
+var cors = require('cors')
 
 const allRoutes = require('./routes');
 
@@ -12,7 +13,7 @@ db.then(() =>{
 .catch((err) =>{
   console.log(err)
 })
-
+app.use(cors())
 app.use(express.json())
 app.use(allRoutes)
 
